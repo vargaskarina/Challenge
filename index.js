16bb182aad975f6037f59d47251df609e6844ca9
vargaskarina <106886192+vargaskarina@users.noreply.github.com>
2022-06-26 19:51:28 -0300

    Comentarios y formato

--- index.js
+++ index.js
@@ -1,87 +1,98 @@
 //Librerías
-const axios = require('axios').default;
+const axios = require("axios").default;
 //Constantes y variables
 const baseUrl = "https://rooftop-career-switch.herokuapp.com";
 
 //Obtiene Token
 async function getToken(email) {
-    try {
-        const response = await axios.get(baseUrl + "/token?email=" + email);
-        const token = response.data.token;
-        return token;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await axios.get(baseUrl + "/token?email=" + email);
+    const token = response.data.token;
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //Obtiene los bloques desordenados
 async function getBlocks(token) {
-    try {
-        const response = await axios.get(baseUrl + "/blocks?token=" + token);
-        const blocksArray = response.data.data;
-        return blocksArray;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await axios.get(baseUrl + "/blocks?token=" + token);
+    const blocksArray = response.data.data;
+    return blocksArray;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //Verifica si dos bloque son consecutivos
 async function check(blocksArray, token) {
-    let orderedBlock = [];
-    try {
-        orderedBlock.push(blocksArray[0]);  //La 1ra posición siempre es correcta, agrego elemento al arreglo ordenado
-        blocksArray.shift();    //Elimino 1er elemento ordenado del arreglo desordenado
+  let orderedBlock = [];
+  try {
+    orderedBlock.push(blocksArray[0]); //La 1ra posición siempre es correcta, agrego elemento al arreglo ordenado
+    blocksArray.shift(); //Elimino 1er elemento ordenado del arreglo desordenado
 
-        while (blocksArray.length != 0) {
-            let block1 = orderedBlock[orderedBlock.length - 1]; //Obtengo último elemento ordenado
+    while (blocksArray.length != 0) {
+      let block1 = orderedBlock[orderedBlock.length - 1]; //Obtengo último elemento ordenado
 
-            for (let i = 0; i < blocksArray.length; i++) {
-                const element = blocksArray[i];
-                let body = { blocks: [block1, element] };   //2do parámetro para llamada POST
-                const response = await axios.post(baseUrl + "/check?token=" + token, body);
-                let message = response.data.message;
-                if (message) {
-                    //Si dos bloques son consecutivos, agrego elemento al arreglo ordenado y lo elimino del desorenado
-                    orderedBlock.push(element);
-                    blocksArray = blocksArray.filter(block => block != element);
-                    break;
-                }
-            }
+      for (let i = 0; i < blocksArray.length; i++) {
+        const element = blocksArray[i];
+        let body = { blocks: [block1, element] }; //2do parámetro para llamada POST
+        const response = await axios.post(
+          baseUrl + "/check?token=" + token,
+          body
+        );
+        let message = response.data.message;
+        if (message) {
+          //Si dos bloques son consecutivos, agrego elemento al arreglo ordenado y lo elimino del desorenado
+          orderedBlock.push(element);
+          blocksArray = blocksArray.filter((block) => block != element);
+          break;
         }
-        return orderedBlock;
-    } catch (error) {
-        console.log(error);
+      }
     }
+    return orderedBlock;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function checkOrder(token, orderedArray){
+//Verifica si el mensaje está ordenado correctamente
+async function checkOrder(token, orderedArray) {
+  try {
     let result = "";
     for (let i = 0; i < orderedArray.length; i++) {
-        result += orderedArray[i];
+      result += orderedArray[i];
     }
-    let body = {encoded: result};
+    let body = { encoded: result }; //2do parámetro para llamada POST
     const response = await axios.post(baseUrl + "/check?token=" + token, body);
     let message = response.data.message;
-    if (message){
-        return true;
-    }else{
-        return false;
+    if (message) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function main() {
-    let userToken = await getToken("karinavargas365@gmail.com");
-    //console.log(userToken);
-    let userArray = await getBlocks(userToken);
-    console.log(userArray);
-    let userOrderedArray = await check(userArray, userToken);
-    console.log(userOrderedArray);
-    let userOrderOk = checkOrder(userToken,userOrderedArray);
-    if (userOrderOk) {
-        console.log("Ordenado");
-    } else {
-        console.log("Desordenado");
-    }
+  let userToken = await getToken("karinavargas365@gmail.com");
+  //console.log(userToken);
+
+  let userArray = await getBlocks(userToken);
+  console.log(userArray);
+
+  let userOrderedArray = await check(userArray, userToken);
+  console.log(userOrderedArray);
+
+  let userOrderOk = checkOrder(userToken, userOrderedArray);
+  if (userOrderOk) {
+    console.log("Ordenado");
+  } else {
+    console.log("Desordenado");
+  }
 }
 
 main();
